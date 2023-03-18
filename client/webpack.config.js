580d9720e9cf57abbfecea086888fcdc2cfca0a2
RufatEskendarov <rufat.eskendarov@gmail.com>
2023-03-18 20:57:39 -0300

    Updated manifest part of webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "JATE",
+        title: "Texter",
       }),
 
       // Injects our custom service worker
@@ -33,9 +33,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
+        name: "Texter",
         short_name: "Contact",
-        description: "Never forget your contacts!",
+        description: "A simple text editor that runs in the browser.",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
